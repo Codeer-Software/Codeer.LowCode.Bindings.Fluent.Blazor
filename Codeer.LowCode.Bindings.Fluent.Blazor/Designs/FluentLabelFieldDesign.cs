880b73d6b97385c5a93e9296f33285dbf30f408c
yutaka <shigure@refy.net>
2025-02-15 03:34:18 +0900

    Add Fluent Label

---
+++ Codeer.LowCode.Bindings.Fluent.Blazor/Designs/FluentLabelFieldDesign.cs
@@ -0,0 +1,11 @@
+﻿using Codeer.LowCode.Bindings.Fluent.Blazor.Components;
+using Codeer.LowCode.Blazor.Repository.Design;
+
+namespace Codeer.LowCode.Bindings.Fluent.Blazor.Designs
+{
+    public class FluentLabelFieldDesign : LabelFieldDesign
+    {
+        public FluentLabelFieldDesign() => TypeFullName = typeof(FluentLabelFieldDesign).FullName!;
+        public override string GetWebComponentTypeFullName() => typeof(FluentLabelFieldComponent).FullName!;
+    }
+}
---
+++ Codeer.LowCode.Bindings.Fluent.Blazor/Extensions/FieldDesignExtension.cs
@@ -0,0 +1,20 @@
+﻿using Codeer.LowCode.Blazor.Repository.Design;
+
+namespace Codeer.LowCode.Bindings.Fluent.Blazor.Extensions
+{
+    internal static class FieldDesignExtensions
+    {
+        internal static string GetDisplayText(this FieldDesignBase field)
+        {
+            if (field is IDisplayName displayName && !string.IsNullOrEmpty(displayName.DisplayName))
+                return displayName.DisplayName;
+            return field.Name;
+        }
+
+        internal static bool IsRequired(this FieldDesignBase field)
+        {
+            if (field is IRequired required) return required.IsRequired;
+            return false;
+        }
+    }
+}

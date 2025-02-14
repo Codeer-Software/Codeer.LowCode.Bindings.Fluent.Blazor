4178d283592acc1307145d8e3c18242d3695a6db
yutaka <shigure@refy.net>
2025-02-15 02:10:30 +0900

    Support fluent anchor

---
+++ Codeer.LowCode.Bindings.Fluent.Blazor/Designs/FluentAnchorTagFieldDesign.cs
@@ -0,0 +1,16 @@
+﻿using Codeer.LowCode.Bindings.Fluent.Blazor.Components;
+using Codeer.LowCode.Blazor.Repository.Design;
+using Microsoft.FluentUI.AspNetCore.Components;
+
+namespace Codeer.LowCode.Bindings.Fluent.Blazor.Designs
+{
+    [IgnoreBaseProperties(nameof(Style))]
+    public class FluentAnchorTagFieldDesign : AnchorTagFieldDesign
+    {
+        [Designer]
+        public Appearance Appearance { get; set; } = Appearance.Hypertext;
+
+        public FluentAnchorTagFieldDesign() => TypeFullName = typeof(FluentAnchorTagFieldDesign).FullName!;
+        public override string GetWebComponentTypeFullName() => typeof(FluentAnchorTagFieldComponent).FullName!;
+    }
+}
--- Example/DesignData/App/Modules/Components.mod.cs
+++ Example/DesignData/App/Modules/Components.mod.cs
@@ -3,3 +3,7 @@ void MudButton_OnClick()
 {
     MessageBox.Show("clicked");
 }
+void FluentAnchorTag_OnClick()
+{
+    MessageBox.Show("clicked");
+}
